449c3e0f268fb45c1b838c048a05d5e9502f1b18
Cheetah555V2 <123528429+Cheetah555V2@users.noreply.github.com>
2025-10-21 12:38:13 +0700

    I'm so dumb 😢

--- hello_world.py
+++ hello_world.py
@@ -7,15 +7,12 @@ index = 0
 
 while curr_word != word:
     for i in " " + string.ascii_letters:
-        time.sleep(0.04)
+        time.sleep(0.03)
         print(curr_word + i)
-        if i == word[index] and word == curr_word + i:
-            curr_word += i
-            index += 1
-            os.system("cls")
-            print(curr_word)
-            break
-        elif i == word[index]:
+        if i == word[index]:
             curr_word += i
             index += 1
             break
+
+os.system("cls")
+print(curr_word)

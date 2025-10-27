d53647660c9e392734d1fc959f816b6222eb99ca
Cheetah555V2 <123528429+Cheetah555V2@users.noreply.github.com>
2025-10-27 20:57:30 +0700

    In progress

---
+++ final_project/final_project.py
@@ -0,0 +1,27 @@
+import os
+#import decimal
+
+
+
+def factoring_number(number):
+    if number <= 1.79699* (10**29): # use O(n^(1/4)) algorithm
+        pass
+    elif 
+
+def ord(modulo, number):
+
+
+def is_prime(number):
+    if number <= 1:
+        return False
+    
+    """
+    AKS primality test (Agrawal-Kayal-Saxena primality test)
+    
+    input : int n > 1
+    1.Check if n is a perfect power, if yes then output False
+        1.1.For a = 2 to log2(n):
+            1.1.1.For b = 2 to log2(n)/log2(a):
+    2.Find the smallest r that ord_r(n) > log2(n)^2
+    """
+

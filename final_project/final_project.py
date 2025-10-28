970400fbc324be78ce9bf1dc110f5efbb4cd16cd
Cheetah555V2 <123528429+Cheetah555V2@users.noreply.github.com>
2025-10-28 20:27:29 +0700

    Add a simple primitive test and basic UI

--- final_project/final_project.py
+++ final_project/final_project.py
@@ -1,27 +1,128 @@
+import math
+import msvcrt
 import os
-#import decimal
+import time
+
+"""
+|==============================================
+|              Console commmand
+|==============================================
+"""
 
 
+def clear_console():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
 
-def factoring_number(number):
-    if number <= 1.79699* (10**29): # use O(n^(1/4)) algorithm
-        pass
-    elif 
 
-def ord(modulo, number):
+def get_char():
+    return chr(msvcrt.getch()[0])
+
+
+"""
+|==============================================
+|                Primitive test
+|==============================================
+"""
+
+
+def trial_division_primitive_test(number):
+    if number <= 1:
+        return False
+
+    for index in range(2, int(math.sqrt(number))):
+        if number % index == 0:
+            return False
+
+    return True
 
 
 def is_prime(number):
-    if number <= 1:
-        return False
-    
-    """
-    AKS primality test (Agrawal-Kayal-Saxena primality test)
-    
-    input : int n > 1
-    1.Check if n is a perfect power, if yes then output False
-        1.1.For a = 2 to log2(n):
-            1.1.1.For b = 2 to log2(n)/log2(a):
-    2.Find the smallest r that ord_r(n) > log2(n)^2
-    """
+    pass
+
+
+"""
+|==============================================
+|                User Interface
+|==============================================
+"""
+
+
+def UI(page, input_data=None):
+    if page == "menu":
+        print("""RSA encryption system menu
+press the following keys to select an option:
+(1) Checking for primality
+(2) Generating a pair of RSA keys
+(3) Encrypting a message
+(4) Decrypting a message
+(Q) Quit program\
+""")
+    elif page == "primality_test_1":
+        print("Primality Test")
+        print("Enter an integer to test for primality: ")
+    elif page == "primality_test_2":
+        print(f"The number {input_data[0]} is {input_data[1]}")
+        print(f"The program took {input_data[2]:.6f} to complete.\n")
+        print(f"(R) to do primality test again or\n\
+(M) to return to main menu.")
+
+"""
+|==============================================
+|                 Main function
+|==============================================
+"""
+
+while True:
+    clear_console()
+    UI("menu")
+    choice = get_char().lower()
+    if choice == "1":
+
+        while True:
+
+            clear_console()
+            UI("primality_test_1")
+
+            number = int(input())
+            print("Processing... please wait.")
+            start_time = time.time()
+            result = trial_division_primitive_test(number)
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            clear_console()
+
+            if result:
+                UI("primality_test_2", (number, "prime", elapsed_time))
+            else:
+                UI("primality_test_2", (number, "coprime", elapsed_time))
+
+            choice = get_char().lower()
+
+            if choice == "r":
+                continue
+            elif choice == "m":
+                clear_console()
+                break
+            else:
+                print("Invalid input, returning to main menu.")
+                time.sleep(1)
+                clear_console()
+                break
+
+        continue
+
+    elif choice == "2":
+        pass
+
+    elif choice == "3":
+        pass
+
+    elif choice == "4":
+        pass
+
+    elif choice == "q":
+        break
 

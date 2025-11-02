13281fef2eabf7118ad48048be3a7595d01189cf
Cheetah555V2 <123528429+Cheetah555V2@users.noreply.github.com>
2025-11-02 14:05:30 +0700

    asdasd

--- Class work/In-class-task-day5/idk_what_im_doing.py
+++ Class work/In-class-task-day5/idk_what_im_doing.py
@@ -1,1 +1,10 @@
-x=float(input());print("B")if(x%2==0 and x%3!=0)else(print("C")if(x%2==0)else(print("D")))
+#x=float(input());print("B")if(x%2==0 and x%3!=0)else(print("C")if(x%2==0)else(print("D")))
+
+number = float(input())
+
+if (number % 2 == 0 and number % 3 != 0):
+    print("B")
+elif (number % 2 == 0):
+    print("C")
+else:
+    print("D")
